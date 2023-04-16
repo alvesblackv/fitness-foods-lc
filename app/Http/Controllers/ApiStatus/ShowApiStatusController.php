2a48cabfbfb34b14d97a84b5725d5b040329e72b
Marcelo Alves <marcelonralves@gmail.com>
2023-04-16 16:03:53 -0300

    [Feature] - Controllers

---
+++ app/Http/Controllers/ApiStatus/ShowApiStatusController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\ApiStatus;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class ShowApiStatusController extends Controller
+{
+    public function __invoke(): JsonResponse
+    {
+        //
+    }
+}
